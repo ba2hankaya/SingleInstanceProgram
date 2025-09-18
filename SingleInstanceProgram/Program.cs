150a2d3e9d2b49ff4c5f7c5edc472958f9b8726d
ba2hankaya <baa2han.kay@gmail.com>
2025-09-18 18:35:05 +0300

    Added comments

--- SingleInstanceProgram/Program.cs
+++ SingleInstanceProgram/Program.cs
@@ -1,5 +1,6 @@
 ﻿using SingleInstanceProgramNS;
 
+//event handler
 void s_MessageReceived(object? sender, MessageReceivedEventArgs e)
 {
     if (e.Message != null)
@@ -8,6 +9,7 @@ void s_MessageReceived(object? sender, MessageReceivedEventArgs e)
     }
 }
 
+//Process args dummy function
 void PrintStringArray(string[] strings)
 {
     foreach(string s in strings)
@@ -16,9 +18,13 @@ void PrintStringArray(string[] strings)
     }
 }
 
+//Unique ID will be used to establish named mutex and pipes. args is the string[] of arguments that additional instances of the program should send to the initial instance.
 SingleInstanceProgram s = SingleInstanceProgram.GetInstance("UniqueId", args);
-PrintStringArray(args);
-s.MessageReceived += s_MessageReceived;
 
+//Since the initial instance of the program won't trigger MessageReceived event handler, run the function for processing args manually.
+PrintStringArray(args);
+
+//Subscribe to the event
+s.MessageReceived += s_MessageReceived;
 Console.ReadLine();
 s.MessageReceived -= s_MessageReceived;
