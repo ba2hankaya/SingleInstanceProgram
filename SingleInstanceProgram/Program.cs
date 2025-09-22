1dad9d6274230096cc9945517536b538117ac19d
ba2hankaya <baa2han.kay@gmail.com>
2025-09-22 20:13:13 +0300

    Modified to show the usage of new event MessageReceivedFromFirstInstance, changed MessageReceivedEventArgs, and the new Start() function.

--- SingleInstanceProgram/Program.cs
+++ SingleInstanceProgram/Program.cs
@@ -1,30 +1,54 @@
 ﻿using SingleInstanceProgramNS;
 
 //event handler
-void s_MessageReceived(object? sender, MessageReceivedEventArgs e)
+void s_MessageReceivedFromOtherInstance(object? sender, MessageReceivedEventArgs e)
+{
+    //process the string array(commands sent by the other instance)
+    if (e.Message != null)
+    {
+        PrintStringArray(e.Message);
+    }
+
+    //Respond to the sender instance
+    if (e.RespondToOtherSender != null)
+    {
+        e.RespondToOtherSender(["Test123"]);
+    }
+}
+
+void s_MessageReceivedFromFirstInstance(object? sender, MessageReceivedEventArgs e)
 {
     if (e.Message != null)
     {
         PrintStringArray(e.Message);
     }
 }
-
 //Process args dummy function
 void PrintStringArray(string[] strings)
 {
-    foreach(string s in strings)
+    foreach (string s in strings)
     {
         Console.WriteLine(s);
     }
 }
 
 //Unique ID will be used to establish named mutex and pipes. args is the string[] of arguments that additional instances of the program should send to the initial instance.
+#if DEBUG
+SingleInstanceProgram s = SingleInstanceProgram.GetInstance("UniqueId", "1 2 3".Split());
+#else
 SingleInstanceProgram s = SingleInstanceProgram.GetInstance("UniqueId", args);
+#endif
+
+//Subscribe to the events before starting the single instance thread and calling functions that use events
+s.MessageReceivedFromFirstInstance += s_MessageReceivedFromFirstInstance;
+s.MessageReceivedFromOtherInstance += s_MessageReceivedFromOtherInstance;
+
+//Starts the single instance thread and can call functions that use events
+s.Start();
 
 //Since the initial instance of the program won't trigger MessageReceived event handler, run the function for processing args manually.
 PrintStringArray(args);
 
-//Subscribe to the event
-s.MessageReceived += s_MessageReceived;
 Console.ReadLine();
-s.MessageReceived -= s_MessageReceived;
+s.MessageReceivedFromFirstInstance -= s_MessageReceivedFromFirstInstance;
+s.MessageReceivedFromOtherInstance -= s_MessageReceivedFromOtherInstance;
