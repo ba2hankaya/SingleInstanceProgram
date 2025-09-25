c7d33ccd8e7eda7e6f9ebae906afd4d62dc6c8e8
ba2hankaya <baa2han.kay@gmail.com>
2025-09-25 15:02:33 +0300

    Fix naming conventions, add comments for readability

--- SingleInstanceProgram/SingleInstanceProgram.cs
+++ SingleInstanceProgram/SingleInstanceProgram.cs
@@ -7,10 +7,10 @@ namespace SingleInstanceProgramNS
 {
     public class SingleInstanceProgram
     {
-        private readonly string _instanceId;
-        private Mutex? mutex = null;
-        private static SingleInstanceProgram? instance = null;
-        private string[] myArgs;
+        private readonly string _instanceId; //string used for initailizing the named mutex and IPC pipes
+        private Mutex? _mutex;
+        private static SingleInstanceProgram? _singletonInstance = null;
+        private string[] _argsToBeSentToFirstProgramInstance; //Stores the data passed to the constructor of an instance of this class. This will be sent by secondary instances of the program to the first instance of the program.
 
         /// <summary>
         /// Creates initial instance of the class. instanceId takes a unique id to be used in named mutex and pipe server. args takes the data to be sent from additional instances of the program to the initial one.
@@ -19,14 +19,14 @@ namespace SingleInstanceProgramNS
         /// <param name="args"></param>
         /// <returns></returns>
         /// <exception cref="InvalidOperationException"></exception>
-        public static SingleInstanceProgram Create(string instanceId, string[] args)
+        public static SingleInstanceProgram Create(string instanceId, string[] args)  //Singleton pattern so that first instance can't create more instances which would act like secondary instances
         {
-            if(instance != null)
+            if(_singletonInstance != null)
             {
                 throw new InvalidOperationException("Singleton already created - use getinstance()");
             }
-            instance = new SingleInstanceProgram(instanceId, args);
-            return instance;
+            _singletonInstance = new SingleInstanceProgram(instanceId, args);
+            return _singletonInstance;
         }
 
         /// <summary>
@@ -34,29 +34,35 @@ namespace SingleInstanceProgramNS
         /// </summary>
         /// <returns></returns>
         /// <exception cref="InvalidOperationException"></exception>
-        public static SingleInstanceProgram GetInstance()
+        public static SingleInstanceProgram GetInstance()  //Singleton pattern so that first instance can't create more instances which would act like secondary instances
         {
-            if (instance == null)
+            if (_singletonInstance == null)
             {
                 throw new InvalidOperationException("Singleton not created - use GetInstance(arg1, arg2)");
             }
-            return instance;
+            return _singletonInstance;
         }
         private SingleInstanceProgram(string instanceId, string[] args)
         {
             _instanceId = instanceId;
-            myArgs = args;
+            _argsToBeSentToFirstProgramInstance = args;
         }
 
         /// <summary>
-        /// Starts the single instance thread and calls functions that use events, event should be subscribed to before calling this method.
+        /// Starts the single instance thread and calls functions that use events, event should be subscribed to before calling this method. Set isGlobal to true to make the system wide mutex global.(This will block other users on the same machine from creating first instances.)
         /// </summary>
-        public void Start()
+        /// <param name="isGlobal"></param>
+        /// 
+
+        //This function defines a mutex using _instanceId and tries to own it. If it can't (if another instance of the program exists), sends myArgs to the first instance
+        //If it can (if another instance of the program doesn't exist) it will start a new background thread for listening to secondary instances of the program (to listen for input).
+        public void Start(bool isGlobal = false)
         {
-            mutex = new Mutex(false, $"Local\\{_instanceId}");
-            if (!mutex.WaitOne(0, false))
+            string mutexPre = isGlobal ? @"Global\" : @"Local\";
+            _mutex = new Mutex(false, mutexPre + _instanceId);
+            if (!_mutex.WaitOne(0, false))
             {
-                SendToFirstInstance(myArgs);
+                SendToFirstInstance(_argsToBeSentToFirstProgramInstance);
                 Environment.Exit(0);
             }
 
@@ -66,6 +72,9 @@ namespace SingleInstanceProgramNS
             listenerThread.Start();
         }
 
+        /* The method for the listener thread. Opens a two-way named pipe server stream with _instanceId, and checks for connections from other instances of the program forever.
+         * Once a connection is received, it invokes the MessageReceivedFromOtherInstance event, exposing the received message and a function for responding back to the connecting instance as the arguments of the event.
+        */
         private void ListenForClients()
         {
             while (true)
@@ -79,12 +88,14 @@ namespace SingleInstanceProgramNS
                         if (message != null)
                         {
                             MessageReceivedEventArgs eventArgs = new MessageReceivedEventArgs();
-                            eventArgs.Message = message.Split("~#$");
+                            eventArgs.Message = message.Split("~#$"); //use a special key for splitting and joining messages while communicating so strings with whitespaces can be sent as is.
+                            
+                            //This function is for writing back to the connecting secondary instance. Done as a lambda action so that the writer stream is not exposed to the user of the library.
                             Action<string[]> s = (string[] args) => 
                             {
                                 using (var writer = new StreamWriter(server))
                                 {
-                                    writer.WriteLine(string.Join("~#$", args));
+                                    writer.WriteLine(string.Join("~#$", args));  //use a special key for splitting and joining messages while communicating so strings with whitespaces can be sent as is.
                                     writer.Flush();
                                 }
                             };
@@ -96,6 +107,10 @@ namespace SingleInstanceProgramNS
             }
         }
 
+        /* This method is used by secondary instances of the program to send data to the first instance. It uses a two-way named pipe client stream so it can receive back data from the first instance as well.
+         * Once connected, it will send the provided string array to the first instance. If the first instance responses, it will then invoke MessageReceivedFromFirstInstance event so that user can handle the data
+         * from the second instance.
+         */
         private void SendToFirstInstance(string[] args)
         {
             if (args.Length > 0)
@@ -103,9 +118,9 @@ namespace SingleInstanceProgramNS
                 using (var client = new NamedPipeClientStream(".",_instanceId,PipeDirection.InOut))
                 {
                     client.Connect(200);
-                    using (var writer = new StreamWriter(client, leaveOpen: true))
+                    using (var writer = new StreamWriter(client, leaveOpen: true)) //leave the stream open so that the reader can be initialized on the same stream.
                     {
-                        writer.WriteLine(string.Join("~#$", args));
+                        writer.WriteLine(string.Join("~#$", args));  //use a special key for splitting and joining messages while communicating so strings with whitespaces can be sent as is.
                         writer.Flush();
                     }
                     using (var reader = new StreamReader(client))
@@ -113,8 +128,8 @@ namespace SingleInstanceProgramNS
                         string? message = reader.ReadLine();
                         if (message != null)
                         {
-                            MessageReceivedEventArgs eventArgs = new MessageReceivedEventArgs();
-                            eventArgs.Message = message.Split("~#$");
+                            MessageReceivedEventArgs eventArgs = new MessageReceivedEventArgs();  
+                            eventArgs.Message = message.Split("~#$");  //use a special key for splitting and joining messages while communicating so strings with whitespaces can be sent as is.
                             OnMessageReceivedFromFirstInstance(eventArgs);
                         }
                     }
@@ -132,8 +147,10 @@ namespace SingleInstanceProgramNS
             MessageReceivedFromFirstInstance?.Invoke(this, e);
         }
 
+        //Exposed event so that user can handle how the first instance should handle arguments sent by a secondary instance
         public event EventHandler<MessageReceivedEventArgs>? MessageReceivedFromOtherInstance;
 
+        //Exposed event so that user can handle how secondary instances should handle responses sent back by the first instance
         public event EventHandler<MessageReceivedEventArgs>? MessageReceivedFromFirstInstance;
     }
 
